58beaeee3017ab5f72f732e45a36763616e00438
ZyX <kp-pav@ya.ru>
2014-03-01 23:51:55 +0400

    First working version of expression parser
    WARNING: part of expr.c after //FIXME!!! must be deleted after I figure out how
             to properly do tests.
    
    TODO: tests, documentation, command-line parser (expr.c holds only expression
          parser)
    
    Ref #170

---
+++ src/nvim/expr.h
@@ -0,0 +1,103 @@
+// vim: ts=8 sts=2 sw=2 tw=80
+
+#ifndef NEOVIM_EXPR_H_
+#define NEOVIM_EXPR_H_
+
+#include "nvim/types.h"
+
+typedef enum {
+  kTypeUnknown,
+
+  // Ternary operators
+  kTypeTernaryConditional,    // ? :
+
+  // Binary operators
+  kTypeLogicalOr,             // ||
+  kTypeLogicalAnd,            // &&
+  kTypeGreater,               // >
+  kTypeGreaterThenOrEqualTo,  // >=
+  kTypeLess,                  // <
+  kTypeLessThenOrEqualTo,     // <=
+  kTypeEquals,                // ==
+  kTypeNotEquals,             // !=
+  kTypeIdentical,             // is
+  kTypeNotIdentical,          // isnot
+  kTypeMatches,               // =~
+  kTypeNotMatches,            // !~
+  kTypeAdd,                   // +
+  kTypeSubstract,             // -
+  kTypeStringConcat,          // .
+  kTypeMultiply,              // *
+  kTypeDivide,                // /
+  kTypeModulo,                // %
+
+  // Unary operators
+  kTypeNot,                   // !
+  kTypeMinus,                 // -
+  kTypePlus,                  // +
+
+  // Simple value nodes
+  kTypeNumber,                // 0
+  kTypeFloat,                 // 0.0, 0.0e0
+  kTypeDoubleQuotedString,    // "abc"
+  kTypeSingleQuotedString,    // 'abc'
+  kTypeOption,                // &option
+  kTypeRegister,              // @r
+  kTypeEnvironmentVariable,   // $VAR
+
+  // Curly braces names parts
+  kTypeVariableName,          // Top-level part
+  kTypeSimpleVariableName,    // Variable name without curly braces
+  kTypeIdentifier,            // plain string part
+  kTypeCurlyName,             // curly brace name
+
+  // Complex value nodes
+  kTypeExpression,            // (expr)
+  kTypeList,                  // [expr, ]
+  kTypeDictionary,            // {expr : expr, }
+
+  // Subscripts
+  kTypeSubscript,             // expr[expr:expr]
+  kTypeConcatOrSubscript,     // expr.name
+  kTypeCall,                  // expr(expr, )
+
+  kTypeEmptySubscript,        // empty lhs or rhs in [lhs:rhs]
+} ExpressionType;
+
+// Defines whether to ignore case:
+//    ==   kOperatorUseOption
+//    ==#  kOperatorMatchCase
+//    ==?  kOperatorIgnoreCase
+typedef enum {
+  kOperatorUseOption = 0, // 0 for alloc_clear
+  kOperatorMatchCase,
+  kOperatorIgnoreCase,
+} OperatorType;
+
+typedef struct expression_node {
+  ExpressionType type;
+  // Position of start inside a line.
+  char_u *position;
+  // Only valid for value nodes and kTypeConcatOrSubscript.
+  char_u *end_position;
+  // Only valid for kType(Greater|Less)*, kType[Not]Matches, kType[Not]Equals: 
+  // determines whether case should be ignored
+  OperatorType ignore_case;
+  // Only valid for operators, subscripts (except for kTypeConcatOrSubscript) 
+  // and complex value nodes: represents operator arguments, nodes inside 
+  // a list, …
+  struct expression_node *subnode;
+  // Subnodes are arranged in a linked list.
+  struct expression_node *next_subnode;
+} ExpressionNode;
+
+typedef struct error {
+  char *message;
+  char_u *position;
+} ExpressionParserError;
+
+#ifdef INCLUDE_GENERATED_DECLARATIONS
+# include "expr.h.generated.h"
+#endif
+
+#endif // NEOVIM_EXPR_H_
