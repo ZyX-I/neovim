f49d6249b559da9b167a31d45cd5c82b86e8bf94
ZyX <kp-pav@ya.ru>
2014-03-02 12:53:10 +0400

    Automatically import headers by using cpp to preparse them

---
+++ test/unit/helpers.lua
@@ -0,0 +1,35 @@
+local ffi = require('ffi')
+
+-- load neovim shared library
+local libnvim = ffi.load('./build/src/libnvim-test.so')
+
+-- Luajit ffi parser only understands function signatures.
+-- This helper function normalizes headers, passes to ffi and returns the
+-- library pointer
+local cimport = function(path)
+  if path then
+    local pipe = io.popen('cpp -P ' .. path)
+    local header = pipe:read("*a")
+    pipe:close()
+    ffi.cdef(header)
+  end
+  return libnvim
+end
+
+-- take a pointer to a C-allocated string and return an interned
+-- version while also freeing the memory
+local internalize = function(cdata)
+  ffi.gc(cdata, ffi.C.free)
+  return ffi.string(cdata)
+end
+
+return {
+  cimport = cimport,
+  internalize = internalize,
+  eq = function(expected, actual)
+    return assert.are.same(expected, actual)
+  end,
+  ffi = ffi
+}
+
+-- vim: sw=2 sts=2 tw=80
