794cc6118eddc32a3e0eb9c875a59742d8bb7727
ZyX <kp-pav@ya.ru>
2014-03-02 12:04:56 +0400

    Make it not lose part of variable name after last curly brace and s: prefix
    Previously in `{abc}def` `def` was lost, in `s:{abc}` `s:` was lost (but not in
    `s:abc{def}`)

--- src/nvim/expr.c
+++ src/nvim/expr.c
@@ -158,6 +158,9 @@ static int parse_name(char_u **arg,
 
     p = find_id_end(arg);
 
+    if (p == NULL && len)
+      p = *arg - 1;
+
     if (**arg != '{') {
       if (p == NULL) {
         // FIXME Proper error message
@@ -175,7 +178,8 @@ static int parse_name(char_u **arg,
     (*next_node)->children = parse1_node;
     next_node = &((*next_node)->next);
     *arg = parse1_arg + 1;
-    p = NULL;
+    s = *arg;
+    p = find_id_end(arg);
   }
 
   while (**arg == '{') {
@@ -203,6 +207,11 @@ static int parse_name(char_u **arg,
     next_node = &((*next_node)->next);
     s = *arg;
     p = find_id_end(arg);
+  }
+
+  if (p != NULL) {
+    VALUE_NODE(kTypeIdentifier, error, next_node, s, p)
+    next_node = &((*next_node)->next);
   }
 
   return OK;
