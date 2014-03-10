00262ba0cb29d8f0ddb9baa494ae12e05315d999
ZyX <kp-pav@ya.ru>
2014-03-10 18:19:25 +0400

    More parsing: find command

--- src/nvim/cmd.c
+++ src/nvim/cmd.c
@@ -21,13 +21,45 @@
 # include "cmd.c.generated.h"
 #endif
 
+// Table used to quickly search for a command, based on its first character.
+static CommandType cmdidxs[27] =
+{
+  kCmdAppend,
+  kCmdBuffer,
+  kCmdChange,
+  kCmdDelete,
+  kCmdEdit,
+  kCmdFile,
+  kCmdGlobal,
+  kCmdHelp,
+  kCmdInsert,
+  kCmdJoin,
+  kCmdK,
+  kCmdList,
+  kCmdMove,
+  kCmdNext,
+  kCmdOpen,
+  kCmdPrint,
+  kCmdQuit,
+  kCmdRead,
+  kCmdSubstitute,
+  kCmdT,
+  kCmdUndo,
+  kCmdVglobal,
+  kCmdWrite,
+  kCmdXit,
+  kCmdYank,
+  kCmdZ,
+  kCmdBang
+};
+
 static CommandNode *cmd_alloc(CommandType type)
   FUNC_ATTR_WARN_UNUSED_RESULT FUNC_ATTR_NONNULL_RET
 {
   // XXX May allocate less space then needed to hold the whole struct: less by 
   // one size of CommandArg.
   size_t size = sizeof(CommandNode)
-              + (sizeof(CommandArg) * (cmddefs[type].num_args));
+              + (sizeof(CommandArg) * (CMDDEF(type).num_args));
   CommandNode *node = (CommandNode *) xcalloc(size, 1);
 
   if (node != NULL)
@@ -105,15 +137,17 @@ static void free_address_followup(AddressFollowup *followup)
   vim_free(followup);
 }
 
-static void free_range_data(Range *range)
+static void free_range_data(Range *range, int onlystart)
 {
   if (range == NULL)
     return;
 
   free_address_data(&(range->start));
-  free_address_data(&(range->end));
   free_address_followup(range->start_followups);
-  free_address_followup(range->end_followups);
+  if (!onlystart) {
+    free_address_data(&(range->end));
+    free_address_followup(range->end_followups);
+  }
 }
 
 static void free_cmd_arg(CommandArg *arg, CommandArgType type)
@@ -202,16 +236,18 @@ static void free_cmd_arg(CommandArg *arg, CommandArgType type)
 
 void free_cmd(CommandNode *node)
 {
-  size_t numargs = cmddefs[node->type].num_args;
+  size_t numargs = CMDDEF(node->type).num_args;
   size_t i;
 
   if (node == NULL)
     return;
 
-  for (i = 0; i < numargs; i++)
-    free_cmd_arg(&(node->args[i]), cmddefs[node->type].arg_types[i]);
+  if (node->type != kCmdUnknown)
+    for (i = 0; i < numargs; i++)
+      free_cmd_arg(&(node->args[i]), CMDDEF(node->type).arg_types[i]);
 
-  free_range_data(&(node->range));
+  free_range_data(&(node->range), FALSE);
+  vim_free(node->name);
   vim_free(node);
 }
 
@@ -419,20 +455,136 @@ static int create_error_node(CommandNode **node, CommandParserError *error,
   FUNC_ATTR_NONNULL_ALL FUNC_ATTR_WARN_UNUSED_RESULT
 {
   if (error->message != NULL) {
-    if ((*node = cmd_alloc(kCmdSyntaxError)) == NULL)
+    char_u *line;
+
+    if ((line = vim_strsave(s)) == NULL)
       return FAIL;
+    if ((*node = cmd_alloc(kCmdSyntaxError)) == NULL) {
+      vim_free(line);
+      return FAIL;
+    }
     (*node)->args[ARG_ERROR_LINE].arg.line = line;
     (*node)->args[ARG_ERROR_COLUMN].arg.col =
         col + ((colnr_T) (error->position - s));
     (*node)->args[ARG_ERROR_MESSAGE].arg.str = error->message;
-    (*node)->args[ARG_ERROR_LINESTR].arg.str = vim_strsave(s);
+    (*node)->args[ARG_ERROR_LINESTR].arg.str = line;
   }
+  return OK;
+}
+
+/*
+ * Check if *p is a separator between Ex commands.
+ * Return NULL if it isn't, (p + 1) if it is.
+ */
+char_u *check_nextcmd(char_u *p)
+{
+  p = skipwhite(p);
+  if (*p == '|' || *p == '\n')
+    return p + 1;
+  else
+    return NULL;
+}
+
+/*
+ * Find an Ex command by its name, either built-in or user.
+ * User command name can be found in *name. It is not resolved
+ * Built in command name is not returned, use CMDDEF(type).name
+ * Returns OK or FAIL, may set fields in CommandParserError in case of FAIL
+ */
+static int find_command(char_u **pp, CommandType *type, char_u **name,
+                        CommandParserError *error)
+{
+  size_t len;
+  char_u *p;
+  size_t i;
+  CommandType cmdidx = kCmdUnknown;
+
+  *name = NULL;
+
+  /*
+   * Isolate the command and search for it in the command table.
+   * Exceptions:
+   * - the 'k' command can directly be followed by any character.
+   * - the 's' command can be followed directly by 'c', 'g', 'i', 'I' or 'r'
+   *	    but :sre[wind] is another command, as are :scrip[tnames],
+   *	    :scs[cope], :sim[alt], :sig[ns] and :sil[ent].
+   * - the "d" command can directly be followed by 'l' or 'p' flag.
+   */
+  p = *pp;
+  if (*p == 'k') {
+    *type = kCmdK;
+    p++;
+  } else if (p[0] == 's'
+             && ((p[1] == 'c' && p[2] != 's' && p[2] != 'r'
+                  && p[3] != 'i' && p[4] != 'p')
+                 || p[1] == 'g'
+                 || (p[1] == 'i' && p[2] != 'm' && p[2] != 'l' && p[2] != 'g')
+                 || p[1] == 'I'
+                 || (p[1] == 'r' && p[2] != 'e'))) {
+    *type = kCmdSubstitute;
+    p++;
+  } else {
+    while (ASCII_ISALPHA(*p))
+      p++;
+    // for python 3.x support ":py3", ":python3", ":py3file", etc.
+    if ((*pp)[0] == 'p' && (*pp)[1] == 'y')
+      while (ASCII_ISALNUM(*p))
+        p++;
+
+    // check for non-alpha command
+    if (p == *pp && vim_strchr((char_u *)"@*!=><&~#", *p) != NULL)
+      p++;
+    len = (size_t)(p - *pp);
+    if (**pp == 'd' && (p[-1] == 'l' || p[-1] == 'p')) {
+      // Check for ":dl", ":dell", etc. to ":deletel": that's
+      // :delete with the 'l' flag.  Same for 'p'.
+      for (i = 0; i < len; ++i)
+        if ((*pp)[i] != ((char_u *)"delete")[i])
+          break;
+      if (i == len - 1)
+        --len;
+    }
+
+    if (ASCII_ISLOWER(**pp))
+      cmdidx = cmdidxs[*pp - 'a'];
+    else
+      cmdidx = cmdidxs[26];
+
+    for (; (int)cmdidx < (int)kCmdSIZE;
+         cmdidx = (CommandType)((int)cmdidx + 1))
+      if (STRNCMP(CMDDEF(cmdidx).name, (char *)(*p), (size_t)len) == 0)
+        break;
+
+    // Look for a user defined command as a last resort.
+    if (cmdidx == kCmdSIZE && **pp >= 'A' && **pp <= 'Z') {
+      // User defined commands may contain digits.
+      while (ASCII_ISALNUM(*p))
+        p++;
+      if (p == *pp)
+        cmdidx = kCmdUnknown;
+      if ((*name = vim_strnsave(*pp, p - *pp)) == NULL)
+        return FAIL;
+      *type = kCmdUSER;
+    } else if (p == eap->cmd) {
+      *type = kCmdUnknown;
+      // FIXME proper message
+      error->message = "failed to find command";
+      error->position = *pp;
+      return FAIL;
+    } else {
+      *type = cmdidx;
+    }
+  }
+
+  *pp = p;
+
   return OK;
 }
 
 static int parse_one_cmd(char_u **pp,
                          CommandNode **node,
                          int sourcing,
+                         int exmode_active,
                          linenr_T line,
                          colnr_T col,
                          line_getter fgetline,
@@ -441,9 +593,14 @@ static int parse_one_cmd(char_u **pp,
 {
   CommandNode **next_node = node;
   CommandParserError error;
+  CommandType type = kCmdUnknown;
   Range range;
   char_u *p;
   char_u *s = *pp;
+  char_u *nextcmd = NULL;
+  char_u *name = NULL;
+  char_u *range_start = NULL;
+  int bang = FALSE;
   size_t len;
   size_t i;
 
@@ -456,32 +613,49 @@ static int parse_one_cmd(char_u **pp,
       return FAIL;
     p = *pp + 2;
     len = STRLEN(p);
-    (*next_node)->args[0].arg.str = vim_strnsave(p, len);
+    if (((*next_node)->args[0].arg.str = vim_strnsave(p, len)) == NULL)
+      return FAIL;
     *pp = p + len;
     return OK;
   }
+
   for (;;) {
     // 1. skip comment lines and leading white space and colons
     p = *pp;
     while (*p == ' ' || *p == '\t' || *p == ':')
       p++;
-    // FIXME investigate WTF was this:
-    // in ex mode, an empty line works like :+
-    if (*p == NUL /*&& exmode_active && getline_equal(…)*/)
-      ;
+    // in ex mode, an empty line works like :+ (switch to next line)
+    if (*p == NUL && exmode_active) {
+      AddressFollowup *fw;
+      if ((*next_node = cmd_alloc(kCmdHashbangComment)) == NULL)
+        return FAIL;
+      (*next_node)->range.start.type = kAddrCurrent;
+      if ((fw = address_followup_alloc(type)) == NULL) {
+        free_cmd(*next_node);
+        *next_node = NULL;
+        return FAIL;
+      }
+      fw->type = kAddressFollowupShift;
+      fw->data.shift = 1;
+      (*next_node)->range.start_followups = fw;
+      return OK;
+    }
 
     if (*p == '"') {
       if ((*next_node = cmd_alloc(kCmdComment)) == NULL)
         return FAIL;
       p++;
       len = STRLEN(p);
-      (*next_node)->args[0].arg.str = vim_strnsave(p, len);
+      if (((*next_node)->args[0].arg.str = vim_strnsave(p, len)) == NULL)
+        return FAIL;
       *pp = p + len;
       return OK;
     }
 
-    if (*p == NUL)
+    if (*p == NUL) {
+      *pp = p;
       return OK;
+    }
 
     // FIXME Why was this here?
     /* if (VIM_ISDIGIT(*ea.cmd)) { */
@@ -489,30 +663,30 @@ static int parse_one_cmd(char_u **pp,
     /* } */
 
     // 2. handle command modifiers.
-    CommandType type = kCmdComment;  // It cannot be comment
     for (i = 0; i < (size_t) kCmdSIZE; i++) {
-      if (cmddefs[i].cmd_argt & ISMODIFIER) {
+      if (CMDDEF(i).flags & ISMODIFIER) {
         size_t common_len;
         if (i > 0) {
-          char_u *cmd_name = cmddefs[i].cmd_name;
-          char_u *prev_cmd_name = cmddefs[i - 1].cmd_name;
+          char_u *name = CMDDEF(i).name;
+          char_u *prev_name = CMDDEF(i - 1).name;
           common_len = 0;
           // FIXME: Precompute and record this in cmddefs
-          while (cmd_name[common_len] == prev_cmd_name[common_len])
+          while (name[common_len] == prev_name[common_len])
             common_len++;
         } else {
           common_len = 1;
         }
-        if (checkforcmd(&p, cmddefs[i].cmd_name, common_len)) {
+        if (checkforcmd(&p, CMDDEF(i).name, common_len)) {
           type = (CommandType) i;
           break;
         }
       }
     }
-    if (type != kCmdComment) {
+    if (type != kCmdUnknown) {
       if ((*next_node = cmd_alloc(type)) == NULL)
         return FAIL;
       next_node = &((*next_node)->args[0].arg.cmd);
+      type = kCmdUnknown;
     } else {
       break;
     }
@@ -537,17 +711,22 @@ static int parse_one_cmd(char_u **pp,
   /* repeat for all ',' or ';' separated addresses */
   for (;;) {
     p = skipwhite(p);
+    if (range_start == NULL)
+      range_start = p;
     if (get_address(&p, &range.end, &error) == FAIL) {
       create_error_node(next_node, &error, line, col, s);
+      free_range_data(&range, FALSE);
       return FAIL;
     }
     if (get_address_followups(&p, &error, &range.end_followups) == FAIL) {
       create_error_node(next_node, &error, line, col, s);
+      free_range_data(&range, FALSE);
       return FAIL;
     }
     p = skipwhite(p);
     if (range.end_followups != NULL) {
-      range.end.type = kAddrCurrent;
+      if (range.end.type == kAddrMissing)
+        range.end.type = kAddrCurrent;
     } else if (range.end.type == kAddrMissing) {
       if (*p == '%') {
         range.start.type = kAddrFixed;
@@ -564,6 +743,7 @@ static int parse_one_cmd(char_u **pp,
     }
     if (*p == ';' || *p == ',') {
       range.setpos = (*p == ';');
+      free_range_data(range, TRUE);
       range.start = range.end;
       range.start_followups = range.end_followups;
       range.end.type = kAddrMissing;
@@ -572,6 +752,95 @@ static int parse_one_cmd(char_u **pp,
       break;
     }
   }
+
+  // 4. parse command
+
+  // Skip ':' and any white space
+  while (*p == ' ' || *p == '\t' || *p == ':')
+    p++;
+
+  /*
+   * If we got a line, but no command, then go to the line.
+   * If we find a '|' or '\n' we set ea.nextcmd.
+   */
+  if (*p == NUL || *p == '"' || (nextcmd = check_nextcmd(p)) != NULL) {
+    /*
+     * strange vi behaviour:
+     * ":3"		jumps to line 3
+     * ":3|..."	prints line 3
+     * ":|"		prints current line
+     */
+    if (*p == '|' || (exmode_active && range.end.type != kAddrMissing)) {
+      if ((*next_node = cmd_alloc(kCmdPrint)) == NULL) {
+        free_range_data(&range, FALSE);
+        return FAIL;
+      }
+      (*next_node)->range = range;
+      p++;
+      *pp = p;
+      return OK;
+    } else if (*p == '"') {
+      free_range_data(&range, FALSE);
+      if ((*next_node = cmd_alloc(kCmdComment)) == NULL)
+        return FAIL;
+      p++;
+      len = STRLEN(p);
+      if (((*next_node)->args[0].arg.str = vim_strnsave(p, len)) == NULL)
+        return FAIL;
+      *pp = p + len;
+      return OK;
+    } else {
+      free_range_data(&range, FALSE);
+
+      *pp = (nextcmd == NULL
+             ? p
+             : nextcmd);
+      return OK;
+    }
+  }
+
+  if (find_command(&p, &type, &name, &error) == FAIL) {
+    free_range_data(&range, FALSE);
+    create_error_node(next_node, &error, line, col, s);
+    return FAIL;
+  }
+
+  // Here used to be :Ni! egg. It was removed
+
+  if (*p == '!') {
+    if (type ==  kCmdUSER || CMDDEF(type).flags & BANG) {
+      p++;
+      bang = TRUE;
+    } else {
+      error.message = e_nobang;
+      error.position = p;
+      create_error_node(next_node, &error, line, col, s);
+      return FAIL;
+    }
+  }
+
+  if (range.start != kAddrMissing || range.end != kAddrMissing) {
+    if (!(type == kCmdUSER || CMDDEF(type).flags & RANGE)) {
+      error.message = e_norange;
+      error.position = range_start;
+      create_error_node(next_node, &error, line, col, s);
+      return FAIL;
+    }
+  }
+
+  // Skip to start of argument.
+  // Don't do this for the ":!" command, because ":!! -l" needs the space.
+  if (type != kCmdBang)
+    p = skipwhite(p);
+
+  if ((*next_node = cmd_alloc(type)) == NULL) {
+    free_range_data(&range, FALSE);
+    return FAIL;
+  }
+  (*next_node)->bang = bang;
+  (*next_node)->range = range;
+  (*next_node)->name = name;
+
   *pp = p;
   return OK;
 }
@@ -579,5 +848,5 @@ static int parse_one_cmd(char_u **pp,
 // FIXME
 void happy()
 {
-  parse_one_cmd(NULL, NULL, 0, 0, 0, NULL, NULL);
+  parse_one_cmd(NULL, NULL, 0, 0, 0, 0, NULL, NULL);
 }
