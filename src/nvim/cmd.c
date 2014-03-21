86a05a8449200b40c63ea51da258f70c968a42ae
ZyX <kp-pav@ya.ru>
2014-03-21 13:53:40 +0400

    Do not overwrite error nodes with subsequent commands

--- src/nvim/cmd.c
+++ src/nvim/cmd.c
@@ -2185,9 +2185,7 @@ CommandNode *parse_cmd_sequence(CommandParserOptions o,
         free_cmd(result);
         return NULL;
       }
-      if (ret == NOTDONE)
-        break;
-      assert(parse_start != line);
+      assert(parse_start != line || ret == NOTDONE);
 
       block_command_node = *next_node;
       while (block_command_node != NULL
@@ -2288,6 +2286,8 @@ CommandNode *parse_cmd_sequence(CommandParserOptions o,
           next_node = &((*next_node)->next);
         }
       }
+      if (ret == NOTDONE)
+        break;
       line = skipwhite(line);
       if (*line == '|' || *line == '\n' || *line == '"')
         line++;
