5277c24e8e545c6eb6b252d8e90712bd8a924e17
ZyX <kp-pav@ya.ru>
2014-03-23 19:50:44 +0400

    Fix crash in node_dump

--- src/nvim/translator/parser/expressions.c
+++ src/nvim/translator/parser/expressions.c
@@ -1810,14 +1810,15 @@ static void node_dump(ExpressionNode *node, char **pp)
       ExpressionNode *child = node->children;
       do {
         node_dump(child, &p);
+        child = child->next;
       } while (child != NULL);
       break;
     }
     case kTypeCurlyName:
     case kTypeExpression: {
-      *p++ = node->type == kTypeExpression ? '(' : '{';
+      *p++ = (node->type == kTypeExpression ? '(' : '{');
       node_dump(node->children, &p);
-      *p++ = node->type == kTypeExpression ? ')' : '}';
+      *p++ = (node->type == kTypeExpression ? ')' : '}');
       break;
     }
     case kTypeList: {
