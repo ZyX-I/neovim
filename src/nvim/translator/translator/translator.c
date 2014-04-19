58b775f56f22a1028505296815cb8da01c1f77f1
ZyX <kp-pav@ya.ru>
2014-04-19 19:00:19 +0400

    Some more code. Also make it use for cycle for :for

--- src/nvim/translator/translator/translator.c
+++ src/nvim/translator/translator/translator.c
@@ -277,7 +277,7 @@ static WFDEC(translate_ex_flags, uint_least8_t exflags)
 
 static WFDEC(translate_number, ExpressionType type, char_u *s, char_u *e)
 {
-  WS("vim.number(")
+  WS("vim.number.new(state, ")
   switch (type) {
     case kExprHexNumber:
     case kExprDecimalNumber: {
@@ -311,7 +311,7 @@ static WFDEC(translate_expr, ExpressionNode *expr)
 {
   switch (expr->type) {
     case kExprFloat: {
-      WS("vim.float(")
+      WS("vim.float.new(state, ")
       W_END(expr->position, expr->end_position)
       WS(")")
       break;
@@ -417,7 +417,9 @@ static WFDEC(translate_expr, ExpressionNode *expr)
       ExpressionNode *current_expr;
       bool reversed = FALSE;
 
-      assert(expr->children != NULL);
+      assert(expr->children != NULL
+             || expr->type == kExprDictionary
+             || expr->type == kExprList);
       switch (expr->type) {
         case kExprDictionary: {
           WS("vim.dict.new(state, ")
@@ -568,14 +570,14 @@ static WFDEC(translate_node, CommandNode *node, size_t indent)
     INDENTVAR(iter_var, "i")
     WS("local ")
     ADDINDENTVAR(iter_var)
-    WS(" = vim.iter_start(state, ")
-    // FIXME dump list
-    WS(")\n")
+    WS("\n")
 
     WINDENT(indent)
-    WS("while (")
+    WS("for _, ")
     ADDINDENTVAR(iter_var)
-    WS(" ~= nil) do\n")
+    WS(" in vim.list.iterator(state, ")
+    CALL(translate_expr, node->args[ARG_FOR_RHS].arg.expr)
+    WS(") do\n")
 
     WINDENT(indent + 1)
     ADDINDENTVAR(iter_var)
@@ -609,7 +611,7 @@ static WFDEC(translate_node, CommandNode *node, size_t indent)
         break;
       }
       case kCmdElseif: {
-        WS("else ")
+        WS("else")
         // fallthrough
       }
       case kCmdIf: {
@@ -706,7 +708,7 @@ static WFDEC(translate_node, CommandNode *node, size_t indent)
 
         if (did_first_if) {
           WINDENT(indent + 1)
-          WS("else ")
+          WS("else")
         }
 
         if (next->args[ARG_REG_REG].arg.reg == NULL) {
