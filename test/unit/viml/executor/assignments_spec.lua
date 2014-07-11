1a021a537f9fee2eb0c67a3204412ff7809708c6
ZyX <kp-pav@yandex.ru>
2014-07-11 20:36:54 +0400

    tests: Use :unlet to fix tests
    Problem: blocks.lua tests are executed after assignments.lua, but require g:a
    variable to be undefined. Assignments tests define this variable though.

--- test/unit/viml/executor/assignments_spec.lua
+++ test/unit/viml/executor/assignments_spec.lua
@@ -9,20 +9,24 @@ describe(':let assignments', function()
   ito('Assigns single value to default scope', [[
     let a = 1
     echo a
+    unlet a
   ]], {1})
   ito('Assigns single value to g: (default) scope', [[
     let g:a = 1
     echo a
+    unlet g:a
   ]], {1})
   ito('Assigns single value to g: scope', [[
     let g:a = 1
     echo g:a
+    unlet g:a
   ]], {1})
   ito('Handles list assignments', [[
     let [a, b, c] = [1, 2, 3]
     echo a
     echo b
     echo c
+    unlet a b c
   ]], {1, 2, 3})
   ito('Handles rest list assignments', [[
     let [a, b; c] = [1, 2, 3, 4]
@@ -60,23 +64,27 @@ describe(':let modifying assignments', function()
     echo a
     let a += 1
     echo a
+    unlet a
   ]], {1, 2})
   ito('Increments single value from g: scope', [[
     let g:a = 1
     echo g:a
     let g:a += 1
     echo g:a
+    unlet g:a
   ]], {1, 2})
   ito('Decrements single value from default scope', [[
     let a = 1
     echo a
     let a -= 1
     echo a
+    unlet a
   ]], {1, 0})
   ito('Decrements single value from g: scope', [[
     let g:a = 1
     echo g:a
     let g:a -= 1
     echo g:a
+    unlet g:a
   ]], {1, 0})
 end)
