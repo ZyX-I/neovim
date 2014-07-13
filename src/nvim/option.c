183488d40962686d33f2d7fb46351fbf488d02f6
ZyX <kp-pav@yandex.ru>
2014-07-13 11:15:55 +0400

    core: Add findoption_len function
    It is like findoption(), but works with non-NUL-terminated strings.

--- src/nvim/option.c
+++ src/nvim/option.c
@@ -32,6 +32,7 @@
 
 #define IN_OPTION_C
 #include <errno.h>
+#include <assert.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <string.h>
@@ -5697,15 +5698,15 @@ static void check_redraw(long_u flags)
 }
 
 /*
- * Find index for option 'arg'.
+ * Find index for option 'arg' that has given length.
  * Return -1 if not found.
  */
-static int findoption(char_u *arg)
+static int findoption_len(const char_u *const arg, const size_t len)
 {
   int opt_idx;
-  char            *s, *p;
-  static short quick_tab[27] = {0, 0};          /* quick access table */
-  int is_term_opt;
+  char *s, *p;
+  static short quick_tab[27] = {0, 0}; /* quick access table */
+  bool is_term_opt;
 
   /*
    * For first call: Initialize the quick-access table.
@@ -5725,26 +5726,30 @@ static int findoption(char_u *arg)
     }
   }
 
+  assert(len > 0);
+
   /*
    * Check for name starting with an illegal character.
    */
   if (arg[0] < 'a' || arg[0] > 'z')
     return -1;
 
-  is_term_opt = (arg[0] == 't' && arg[1] == '_');
+  is_term_opt = (len > 2 && arg[0] == 't' && arg[1] == '_');
   if (is_term_opt)
     opt_idx = quick_tab[26];
   else
     opt_idx = quick_tab[CharOrdLow(arg[0])];
+  // Match full name
   for (; (s = options[opt_idx].fullname) != NULL; opt_idx++) {
-    if (STRCMP(arg, s) == 0)                        /* match full name */
+    if (STRNCMP(arg, s, len) == 0 && s[len] == NUL)
       break;
   }
   if (s == NULL && !is_term_opt) {
     opt_idx = quick_tab[CharOrdLow(arg[0])];
+    // Match short name
     for (; options[opt_idx].fullname != NULL; opt_idx++) {
       s = options[opt_idx].shortname;
-      if (s != NULL && STRCMP(arg, s) == 0)         /* match short name */
+      if (s != NULL && STRNCMP(arg, s, len) == 0 && s[len] == NUL)
         break;
       s = NULL;
     }
@@ -5752,6 +5757,15 @@ static int findoption(char_u *arg)
   if (s == NULL)
     opt_idx = -1;
   return opt_idx;
+}
+
+/*
+ * Find index for option 'arg'.
+ * Return -1 if not found.
+ */
+static int findoption(char_u *arg)
+{
+  return findoption_len(arg, STRLEN(arg));
 }
 
 /*
