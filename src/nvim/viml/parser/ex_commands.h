e3512e1f9465df42a42fbd78e455cbacd7e663d7
ZyX <kp-pav@yandex.ru>
2014-07-22 20:06:51 +0400

    fix! Quick fix to the includes list of parser/ex_commands.h
    This commit should go away once *.h files are properly refactored.

--- src/nvim/viml/parser/ex_commands.h
+++ src/nvim/viml/parser/ex_commands.h
@@ -13,6 +13,9 @@
 #include "nvim/buffer_defs.h"
 #include "nvim/viml/parser/command_definitions.h"
 #include "nvim/viml/parser/expressions.h"
+// FIXME Remove the two following includes: only regexp_defs is needed.
+#include "nvim/vim.h"
+#include "nvim/buffer_defs.h"
 #include "nvim/regexp_defs.h"
 
 // :argadd/:argd
