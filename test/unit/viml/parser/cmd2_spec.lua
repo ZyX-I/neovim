14375bebf9deb1c1f5eb99262ea0de6ebb251d84
ZyX <kp-pav@yandex.ru>
2014-09-07 20:31:57 +0400

    parser/tests: Add tests for :edit command family

--- test/unit/viml/parser/cmd2_spec.lua
+++ test/unit/viml/parser/cmd2_spec.lua
@@ -47,7 +47,8 @@ describe('parse_one_cmd', function()
       prev = 'previous',
       sN = 'sNext',
       sa = 'sargument',
-      spr = 'sprevious'
+      spr = 'sprevious',
+      argu = 'argument',
     }) do
       itn(full, trunc)
       itn(full .. '!', trunc .. '!')
@@ -65,11 +66,88 @@ describe('parse_one_cmd', function()
     return itn('wqall!', 'wqa!')
   end)
   describe(':edit command family', function()
-    itn('edit lit(abc) lit(def)', 'edit  abc  def')
-    itn('edit lit({abc) lit(def)', 'e  {abc  def')
-    itn('edit shell(echo abc).lit(def) lit(ghi)', 'e  `echo abc`def ghi')
-    itn('edit expr("abc").lit(def) lit(ghi)', 'e  `="abc"`def ghi')
-    itn('edit lit(`echo abc)', 'e  `echo\\ abc')
+    for trunc, cmdprops in pairs({
+      e = {full='edit', cmd=true, opts=true, bang=true},
+      vi = {full='visual', cmd=true, opts=true, bang=true},
+      vie = {full='view', cmd=true, opts=true, bang=true},
+      ped = {full='pedit', cmd=true, opts=true, bang=true},
+      ex = {full='ex', cmd=true, opts=true, bang=true},
+      ar = {full='args', cmd=true, opts=true, bang=true},
+      arga = {full='argadd', count=true},
+      argd = {full='argdelete'},
+      arge = {full='argedit', cmd=true, opts=true, count=true, bang=true},
+      argg = {full='argglobal', cmd=true, opts=true, bang=true},
+      argl = {full='arglocal', cmd=true, opts=true, bang=true},
+      cf = {full='cfile'},
+      cg = {full='cgetfile'},
+      lf = {full='lfile'},
+      lg = {full='lgetfile'},
+      cd = {full='cd', bang=true},
+      lcd = {full='lcd', bang=true},
+      chd = {full='chdir', bang=true},
+      lchd = {full='lchdir', bang=true},
+      diffs = {full='diffsplit'},
+      diffp = {full='diffpatch'},
+      exi = {full='exit', opts=true, range=true, bang=true},
+      x = {full='xit', opts=true, range=true, bang=true},
+      fin = {full='find', cmd=true, opts=true, count=true, bang=true},
+      sf = {full='sfind', cmd=true, opts=true, count=true, bang=true},
+      laddf = {full='laddfile'},
+      luafile = {full='luafile', range=true},
+      mzf = {full='mzfile', range=true},
+      pyf = {full='pyfile', range=true},
+      py3f = {full='py3file', range=true},
+      tclf = {full='tclfile', range=true},
+      rubyf = {full='rubyfile', range=true},
+      mk = {full='mkexrc', bang=true},
+      mks = {full='mksession', bang=true},
+      mkv = {full='mkvimrc', bang=true},
+      mkvie = {full='mkview', bang=true},
+      n = {full='next', cmd=true, opts=true, count=true, bang=true},
+      sn = {full='snext', cmd=true, opts=true, count=true, bang=true},
+      new = {full='new', cmd=true, opts=true, count=true, bang=true},
+      vne = {full='vnew', cmd=true, opts=true, count=true, bang=true},
+      sp = {full='split', cmd=true, opts=true, count=true, bang=true},
+      vs = {full='vsplit', cmd=true, opts=true, count=true, bang=true},
+      sv = {full='sview', cmd=true, opts=true, count=true, bang=true},
+      tabe = {full='tabedit', cmd=true, opts=true, count=true, bang=true},
+      tabnew = {full='tabnew', cmd=true, opts=true, count=true, bang=true},
+      tabf = {full='tabfind', cmd=true, opts=true, count=true, bang=true},
+      rec = {full='recover', bang=true},
+      ru = {full='runtime', bang=true},
+      so = {full='source', bang=true},
+      rundo = {full='rundo'},
+      wundo = {full='wundo', bang=true},
+      rv = {full='rviminfo', bang=true},
+      wv = {full='wviminfo', bang=true},
+      sav = {full='saveas', opts=true, bang=true},
+      wn = {full='wnext', opts=true, count=true, bang=true},
+      wN = {full='wNext', opts=true, count=true, bang=true},
+      wp = {full='wprevious', opts=true, count=true, bang=true},
+    }) do
+      full = cmdprops.full
+      for trunc, full in pairs(
+        cmdprops.bang and {[trunc]=full, [trunc .. '!']=full .. '!'} or {[trunc]=full}
+      ) do
+        itn(full .. ' lit(abc) lit(def)', full .. '  abc  def')
+        itn(full .. ' lit({abc) lit(def)', trunc .. '  {abc  def')
+        itn(full .. ' shell(echo abc).lit(def) lit(ghi)', trunc .. '  `echo abc`def ghi')
+        itn(full .. ' expr("abc").lit(def) lit(ghi)', trunc .. '  `="abc"`def ghi')
+        itn(full .. ' lit(`echo abc)', trunc .. '  `echo\\ abc')
+        if cmdprops.count or cmdprops.range then
+          itn('10' .. full .. ' lit(abc) lit(def)', '10' .. trunc .. ' abc def')
+        end
+        if cmdprops.range then
+          itn('/abc/;/def/' .. full .. ' lit(abc) lit(def)', '/abc/;/def/' .. trunc .. ' abc def')
+        end
+        if cmdprops.opts then
+          itn(full .. ' ++bin lit(abc) lit(def)', trunc .. ' ++binary abc def')
+        end
+        if cmdprops.cmd then
+          itn(full .. ' +redraw lit(abc) lit(def)', trunc .. ' +redr abc def')
+        end
+      end
+    end
   end)
 end)
 return describe('parse_cmd_sequence', function()
