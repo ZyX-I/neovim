5102ea1e6bf1649d7c17d6396e7d471698b25312
ZyX <kp-pav@yandex.ru>
2016-09-04 02:25:24 +0300

    eval: Move remaining get_tv_string* functions to eval/typval.c

--- src/nvim/ex_docmd.c
+++ src/nvim/ex_docmd.c
@@ -6705,7 +6705,7 @@ do_exedit (
         int ms = msg_scroll;
 
         if (eap->nextcmd != NULL) {
-          stuffReadbuff(eap->nextcmd);
+          stuffReadbuff((const char *)eap->nextcmd);
           eap->nextcmd = NULL;
         }
 
